52f12326d114edf6db3c323aff5a31435c96f596
Yvesmorel <hackeryves842@gmail.com>
2024-07-09 09:09:29 +0000

    first commit

--- auth.config.ts
+++ auth.config.ts
@@ -12,7 +12,7 @@ export const authConfig = {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
-        return Response.redirect(new URL("/dashboard/products", nextUrl));
+        return Response.redirect(new URL("/dashboard/category", nextUrl));
       }
       return true;
     },
